f0742790e39b546bebbe82086c01366e391cc7ce
bokyun91 <braun@todayhenry.co>
2021-01-04 22:03:32 +0900

    @html, @debug

---
+++ debugging/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	let h1 = '<h1>Hello Heropy</h1>'
+
+	let name = 'Heropy'
+	let index = 0
+</script>
+
+<!-- @html string -> html 코드처럼 사용 -->
+{@html h1}
+
+{@debug index, name}
+<h1 on:click={() => {index += 1}}>Hello {name}!</h1>
