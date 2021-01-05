a799f7ae9c08a0ccd9e2c5dec9491d32df339982
bokyun91 <braun@todayhenry.co>
2021-01-05 21:25:28 +0900

    assignment

---
+++ assignment/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	let name = 'Heropy'
+	let fruits = ['Apple', 'Banana', 'Cherry']
+
+	function assign() {
+		name = 'Neo'
+		fruits.push('Orange')
+	}
+</script>
+
+<button on:click={assign}>Assign!</button>
+
+<h2>name: {name}</h2>
+<h2>fruits: {fruits}</h2>
