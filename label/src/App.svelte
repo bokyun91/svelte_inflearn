9ce05b221d2c658406976993a9f9fc1e5092dc28
bokyun91 <braun@todayhenry.co>
2021-01-06 21:34:44 +0900

    Javascript label

---
+++ label/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	// Svelte가 아닌, JavaScript 문법입니다!
+
+	loop1:
+	for (let i = 1; i < 4; i += 1) {
+		loop2:
+		for (let k = 1; k < 4; k += 1){
+			if (i === 2) {
+				break loop2
+			}
+			console.log(`${i}-${k}`)
+		}
+		console.log('---------')
+	}
+
+	heropy: {
+		console.log('a')
+		break heropy
+		console.log('b')
+	}
+
+	// Svelte 반응성 구문!
+	let count = 0
+	$: {
+		console.log(count)
+	}
+</script>
