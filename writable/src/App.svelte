d53880c7941ca7b7c67ef450e339181ae121154b
bokyun91 <braun@todayhenry.co>
2021-01-13 20:38:59 +0900

    writable store

---
+++ writable/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import WritableMethods from './WritableMethods.svelte'
+
+	let toggle = true
+</script>
+
+<button on:click={() => toggle = !toggle}>
+	Toggle
+</button>
+
+{#if toggle}
+	<WritableMethods />
+{/if}
---
+++ writable/src/WritableMethods.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { count, name } from './store.js'
+  
+  function increase() {
+    $count += 1
+  }
+
+  function changeName() {
+    $name = 'Neo'
+  }
+</script>
+
+<button 
+  on:click={increase}
+  on:click={changeName}>
+  Click me!
+</button>
+
+<h2>{$count}</h2>
+<h2>{$name}</h2>
